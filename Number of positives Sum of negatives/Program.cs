96219e90975e2810299ee766e86478090be92102
Julia Dubas <uliadubas94@gmail.com>
2022-08-07 18:23:50 +0300

    Add project files.

---
+++ Number of positives Sum of negatives/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Number_of_positives_Sum_of_negatives
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int[] newArray = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15};
+
+            int x = 0;
+
+            int sum = 0;
+
+            for (int i = 0; i < newArray.Length; i++)
+            {
+
+                if (newArray[i] > 0)
+                {
+                    x += 1;
+                }
+                
+                else
+                {
+                    sum += newArray[i];
+                }
+                
+            }
+            Console.WriteLine(x);
+
+            Console.WriteLine(sum);
+
+            Console.ReadLine();
+        }
+    }
+}
